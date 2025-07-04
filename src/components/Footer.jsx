3e30c4ffb07bbedb24b5f0ffe490c6ccbbb72b88
Allfitra <alfitrafadjri63122@gmail.com>
2025-07-04 10:58:47 +0700

    update book data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="w-full bg-gray-800 text-white py-1 mt-auto fixed bottom-0 left-0">
-    <div className="container mx-auto text-center text-sm">&copy; {new Date().getFullYear()} Buku Kita. All rights reserved.</div>
+  <footer className="w-full bg-gray-800 text-white py-1 mt-auto bottom-0 left-0">
+    <div className="container mx-auto text-center text-sm">
+      &copy; {new Date().getFullYear()} Buku Kita. All rights reserved.
+    </div>
   </footer>
 );
 
